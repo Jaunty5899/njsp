fa826028687fd390dff6f314f05e05283725cfa0
Jaunty5899 <jaunty5899@gmail.com>
2020-07-01 13:42:55 +0530

    okey file

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var btn=document.getElementById("btn");
 // console.log(btn);
-btn.addEventListener("click",function increment() {
+btn.addEventListener("click",function() {
     btn.innerHTML=(btn.innerHTML*1)+1;
 });
