c0ded27f2a7369d2905e9898158c0813ff9b240c
Jaunty5899 <jaunty5899@gmail.com>
2020-07-01 17:25:17 +0530

    box size increase with click

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let button=document.querySelector("button");
 let count=document.querySelector("span");
+let divedit=document.querySelector("div");
 // console.log(btn);
 button.addEventListener("click",function() {
     count.textContent=+count.textContent+1;
+    console.log(parseInt(divedit.style.width));
+    let size=parseInt(divedit.style.width);
+    divedit.style.width=size+10+'px';
+    divedit.style.height=size+10+'px';
 });
