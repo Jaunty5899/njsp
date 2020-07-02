f7fd0ea986ea535dbaa499f5bed8fa5e40d44cff
Jaunty5899 <jaunty5899@gmail.com>
2020-07-02 19:06:40 +0530

    confused on how to assign keys

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let downbtn = document.querySelector("#down");
 let rightbtn = document.querySelector("#right");
 let count = document.querySelector("span");
 let divedit = document.querySelector("div");
+
 function logger() {
   console.log(divedit.style.marginLeft);
   console.log(divedit.style.marginTop);
@@ -14,49 +15,59 @@ function counter() {
   count.textContent = +count.textContent + 1;
 }
 
-increasebtn.addEventListener("click", function () {
+increasebtn.addEventListener("click", increasefn);
+
+function increasefn() {
   counter();
   let size = parseInt(divedit.style.width);
   divedit.style.width = size + 10 + "px";
   divedit.style.height = size + 10 + "px";
-});
+}
 
-downbtn.addEventListener("click", function () {
+downbtn.addEventListener("click", downfn);
+
+function downfn() {
   counter();
-  logger();
+  // logger();
   let size = parseInt(divedit.style.marginTop) + 10;
   divedit.style.marginTop = size + "px";
-});
+}
 
-rightbtn.addEventListener("click", function () {
+rightbtn.addEventListener("click", rightfn);
+
+function rightfn() {
   counter();
-  logger();
+  // logger();
   let size = parseInt(divedit.style.marginLeft) + 10;
   if (size > 1345) {
     divedit.style.marginLeft = 1340 + "px";
   } else {
     divedit.style.marginLeft = size + "px";
   }
-});
+}
 
-upbtn.addEventListener("click", function () {
+upbtn.addEventListener("click", upfn);
+
+function upfn() {
   counter();
-  logger();
+  // logger();
   let size = parseInt(divedit.style.marginTop) - 10;
   if (size < 5) {
     divedit.style.marginTop = 5 + "px";
   } else {
     divedit.style.marginTop = size + "px";
   }
-});
+}
 
-leftbtn.addEventListener("click", function () {
+leftbtn.addEventListener("click", leftfn);
+
+function leftfn() {
   counter();
-  logger();
+  // logger();
   let size = parseInt(divedit.style.marginLeft) - 10;
   if (size < 0) {
     divedit.style.marginLeft = 0 + "px";
   } else {
     divedit.style.marginLeft = size + "px";
   }
-});
+}
