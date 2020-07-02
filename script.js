fbc169aaec857cad39a550e016c17faaa14402b1
Jaunty5899 <jaunty5899@gmail.com>
2020-07-02 11:56:22 +0530

    move&increase seperate

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let button = document.querySelector("button");
+let button = document.querySelectorAll("button");
 let count = document.querySelector("span");
 let divedit = document.querySelector("div");
 // console.log(btn);
-button.addEventListener("click", function () {
+
+function counter() {
   count.textContent = +count.textContent + 1;
-  divedit.classList.add("moveR");
+}
+
+button[0].addEventListener("click", function () {
+  // count.textContent = +count.textContent + 1;
   //   console.log(parseInt(divedit.style.width));
+  counter();
   let size = parseInt(divedit.style.width);
   divedit.style.width = size + 10 + "px";
   divedit.style.height = size + 10 + "px";
+});
+
+button[1].addEventListener("click", function () {
+  // count.textContent = +count.textContent + 1;
+  counter();
+  let size = parseInt(divedit.style.marginLeft);
+  divedit.style.marginLeft = size + 10 + "px";
 });
