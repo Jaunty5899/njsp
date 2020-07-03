676463452ace0fcef0f0d1e719d34afe13f8c9cd
Jaunty5899 <jaunty5899@gmail.com>
2020-07-03 16:57:13 +0530

    diagonal movement added

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let rightbtn = document.querySelector("#right");
 let count = document.querySelector("span");
 let divedit = document.querySelector("#subject");
 let container = document.querySelector("#container");
+let keyspressed = {};
 
 console.log(container.style.width);
 // function logger() {
@@ -27,7 +28,41 @@ function increasefn() {
 }
 
 document.addEventListener("keydown", function (event) {
-  if (event.keyCode == 39 || event.keyCode == 68) {
+  keyspressed[event.keyCode] = true;
+  console.log(keyspressed);
+  if (
+    (keyspressed[87] && event.keyCode == 65) ||
+    (keyspressed[38] && event.keyCode == 37) ||
+    (keyspressed[65] && event.keyCode == 87) ||
+    (keyspressed[37] && event.keyCode == 38)
+  ) {
+    upfn();
+    leftfn();
+  } else if (
+    (keyspressed[87] && event.keyCode == 68) ||
+    (keyspressed[38] && event.keyCode == 39) ||
+    (keyspressed[68] && event.keyCode == 87) ||
+    (keyspressed[39] && event.keyCode == 38)
+  ) {
+    upfn();
+    rightfn();
+  } else if (
+    (keyspressed[83] && event.keyCode == 65) ||
+    (keyspressed[40] && event.keyCode == 37) ||
+    (keyspressed[65] && event.keyCode == 83) ||
+    (keyspressed[37] && event.keyCode == 40)
+  ) {
+    downfn();
+    leftfn();
+  } else if (
+    (keyspressed[83] && event.keyCode == 68) ||
+    (keyspressed[40] && event.keyCode == 39) ||
+    (keyspressed[68] && event.keyCode == 83) ||
+    (keyspressed[39] && event.keyCode == 40)
+  ) {
+    downfn();
+    rightfn();
+  } else if (event.keyCode == 39 || event.keyCode == 68) {
     rightfn();
     rightbtn.classList.add("highlight");
   } else if (event.keyCode == 37 || event.keyCode == 65) {
@@ -43,6 +78,7 @@ document.addEventListener("keydown", function (event) {
 });
 
 document.addEventListener("keyup", function (event) {
+  delete keyspressed[event.keyCode];
   if (event.keyCode == 39 || event.keyCode == 68) {
     rightbtn.classList.remove("highlight");
   } else if (event.keyCode == 37 || event.keyCode == 65) {
